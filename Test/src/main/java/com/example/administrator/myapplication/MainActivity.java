0c864ab8586eb74f2e1c97b781532e52861da092
tom <765888697@qq.com>
2017-08-31 22:27:48 +0800

    更改

--- Test/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ Test/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.RelativeLayout;
 
 import butterknife.BindView;
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     @Override
