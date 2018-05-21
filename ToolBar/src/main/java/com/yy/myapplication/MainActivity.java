2f10270997647c8a7b88427b68238dd1fcb1398e
YXHTom <765888697@qq.com>
2018-05-21 16:36:42 +0800

    menu学习记录

---
+++ ToolBar/src/main/java/com/yy/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.yy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "toolbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_cycling:
+                Toast.makeText(this, "骑行", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_other:
+                Toast.makeText(this, "其他", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+
+}
