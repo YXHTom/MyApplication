1bacb6a74f07f9c2148233d97f4e9b7e86f5c117
YXHTom <765888697@qq.com>
2018-05-30 14:02:19 +0800

    DrawLayout学习记录

---
+++ BottomNavigationView/src/main/java/com/yy/myapplication/Main2Activity.java
@@ -0,0 +1,18 @@
+package com.yy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+*
+*@author 杨阳
+*@date 2018/5/24
+*
+*/
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
