31b2eb781bea6e1faff9a73386aef5a2e1863354
YXHTom <765888697@qq.com>
2018-06-12 09:22:57 +0800

    测试新特性

---
+++ MaterialCopoment/src/main/java/com/yy/material/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.yy.material;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("shezhi");
+        toolbar.setSubtitle("vdb");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
--- MaterialCopoment/src/main/java/com/yy/material/MainActivity.java
+++ MaterialCopoment/src/main/java/com/yy/material/MainActivity.java
@@ -1,13 +1,25 @@
 package com.yy.material;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
     }
+
+    public void trans(View v) {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, tv, "textview").toBundle());
+    }
+
 }
